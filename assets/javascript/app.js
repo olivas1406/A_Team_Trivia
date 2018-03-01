ee4d9b21e3e083145754fa45be575e2d062eed2e
olivas1406 <olivas1406@msn.com>
2018-03-01 02:37:02 -0700

    basic HTML is done, 10 second timer after start button press works, no idea what type of trivia to use

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,30 @@
 
 
+function countD() {                                     // Function to count down
+    var count = 10;                                     // start counting at 30
+    var counter=setInterval(timer, 1000);               // set interval to 1 second
+    function timer() {                                  // timer function
+        count = count-1;                                // decrement var count by 1
+            if (count === 0) {                          // if the count gets to 0
+            clearInterval(counter);                     // clear the interval
+            $(".timer").html("<h2>Time's Up!</h2>");    // push 'time's up' to the HTML
+                                                        //
+                                                        // CALL FUNCTION TO TRANSITION TO THE NEXT SCREEN
+            return;
+            }
+            // } else if (click on a, b, c, or d) {
+            // CALL FUNCTION TO TRANSITON TO THE NEXT SCREEN 
+            // }
+        $(".timer").html("Time Remaining: " + count);
+    }
+}
+
+
+
+
+$(".startB").on("click", function() {
+    countD();  
+});
 
 
 
