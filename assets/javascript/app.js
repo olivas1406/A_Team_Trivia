c3d0fff5900d85b06ae38c277970c575cb86af07
olivas1406 <olivas1406@msn.com>
2018-03-03 21:38:02 -0700

    started fresh, cycles through, needs clicks for answers, need solution for pushing questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,296 +1,79 @@
 
-$(document).ready(function(){      
 
-    var questNum = 0            // variable to hold the current question number
-    var unAns = 0               // variable to hold the number of unanswered questions
-    var corAns = 0              // variable to hold the number of correclty answered questions
-    var notCorAns = 0           // variable to hold the number of incorrectly answered questions
+    var currQuest = ["this is test question 1", "this is test question 2", "this is test question 3"];
+    var answer1 = ["answer 1-1", "answer 1-2", "answer 1-3"];
+    var answer2 = ["answer 2-1", "answer 2-2", "answer 2-3"];
+    var answer3 = ["answer 3-1", "answer 3-2", "answer 3-3"];
+    var answer4 = ["answer 4-1", "answer 4-2", "answer 4-3"];
+    var unAns = 0;               // variable to hold the number of unanswered questions
+    var corAns = 0;              // variable to hold the number of correclty answered questions
+    var notCorAns = 0;         // variable to hold the number of incorrectly answered questions
+    var ansYes = 1; 
 
+$(document).ready(function(){ 
 
-
-    
-
-
-
-
-    function countD() {                                     // Function to count down
-        var count = 4;                                      // start counting from 30
+    function timeMe() {
+        var count = 8;                                      // start counting from 30, 8 for testing
         var counter=setInterval(timer, 1000);               // set interval to 1 second
         function timer() {                                  // timer function
             count = count-1;                                // decrement var count by 1
                 if (count === 0) {                          // if the count gets to 0
-                clearInterval(counter);                     // clear the interval
-                $(".timer").html("<h2>Time's Up!</h2>");    // push 'time's up' to the HTML
-                $(".ques").empty();         //.html("");                        // remove the question from the HTML
-                $(".ans").empty();          //.html("");                         // remove the answers from the HTML
-                correctAns();                               // call the correctAns function to show correct answer                                      
-            //   return;
+                clearInterval(counter);                     // clearInterval
+                $(".timer").empty();                    // empty the timer div
+                $(".texty").html("Time's up");                      // push 'time's up' to the HTML 
+                $(".ques").empty();                         // empty the ques div
+                $(".ans").empty();                          // empty the ans div
+              
+                correctAns();                               // call the correctAns function
                 }
-                // } else if (click on a, b, c, or d) {
-            
-             // }
-            $(".timer").html("Time Remaining: " + count);
-        
+            $(".timer").html(count);
         }
     }
 
-
-
-
-
-    function createWords() {
-        $(".ques").html("<h2>The question goes here</h2>");
-        $(".text1").html("<h4>Answer 1 goes here</h4>");
-        $(".text1").on("click", function() {
-            correctAns();
-        $(".timer").empty();
-
-            
-        });
-
+    function askMe() {                                      // function to populate the questions/answers to the HTML
+        $(".ques").html(currQuest[0]);
+        $(".text1").html(answer1[0]);
+        $(".text2").html(answer2[0]);
+        $(".text3").html(answer3[0]);
+        $(".text4").html(answer4[0]);
         
-
-        $(".text2").html("<h4>Answer 2 goes here</h4>"); 
-        $(".text2").on("click", function() {
-            alert("you clicked on answer 2")
-        });
-        $(".text3").html("<h4>Answer 3 goes here</h4>");
-        $(".text3").on("click", function() {
-            alert("you clicked on answer 3")
-        });
-        $(".text4").html("<h4>Answer 4 goes here</h4>"); 
-        $(".text4").on("click", function() {
-            alert("you clicked on answer 4")
-        });
     }
 
-
-
-
-    function gameOn() {                                                                         // calls countdown and words
-        questNum++;                                 // increment var questNum by 1
-        countD();                                   // Call the countdown function
-        $(this).delay(999).queue(createWords);      // Delay the question/answers until the countdown pops up
-    }
 
     function correctAns() {
-    
-        var count = 5;                                      // start counting from 5
+        $(".ques").html("correct answer goes here");
+        var count = 6;                                      // start counting from 30, 8 for testing
         var counter=setInterval(timer, 1000);               // set interval to 1 second
         function timer() {                                  // timer function
             count = count-1;                                // decrement var count by 1
                 if (count === 0) {                          // if the count gets to 0
-                clearInterval(counter);     
-                nextScreen();
+                clearInterval(counter);                     // clearInterval
+                $(".ques").empty();
+                $(".timer").empty();
+                timeMe();
+                $(this).delay(999).queue(askMe);
                 }
-            $(".timer").empty();
-            $(".texty").append(".ques").html("------------------------ The correct answer goes here ----------------------")
-            $(".texty").css({
-                "margin-top" : "5%",
-                "background" : "white",
-                "color" : "black"
-            });
+                if (ansYes === 0) {
+                    notCorAns++;
+                } else if (ansYes === 1) {
+                    corAns++;
+                } else {
+                    unAns++;
+                }
         }
     }
+    
 
-    function nextScreen() {
-     $(".r2c1").html("The nextScreen Function has been called")
-        $(".r2c1").css({
-            "background" : "blue",
-            "color" : "black",
-            "text-align" : "center"
-        }) 
-        gameOn();
-    }
+
 
     $(".startB").on("click", function() {   // start button
-        gameOn();                           // call the gameOn function
+        timeMe();
+        $(this).delay(999).queue(askMe);
     });
-    
-
-
-
-
-
 
 
 
 });
-
-
-
-
-
-
-
-
-
-
-/*
-
-1 on start button press
-
-    $(".startB").on("click", function() {
-    gameOn();
-    questNum++
-});
-
-
-
-2 call timer function
-3 call questions and answers with 999 delay function
-   increment var questNum   <-- use var questNum to reference questions/answers
-
-4   ///// MAKE THIS A FUNCTION
-if timer === 0
-        unAns++
-        blank out the html  <-- if you remove the div you have to re-add during correctAns() and nextScreen()
-        corAns() 
-
-    else if correct answer given
-        correctAns++
-        blank out the html
-        correctAns()  
-
-    else if incorrect answer given
-        incorrectAns++
-        blank out the html 
-        notCorAns()   
-///// MAKE THIS A FUNCTION
-
-//////////////////////////////////////////////////////////
-function correctAns() {
-    var count = 7;                                     // start counting at 30
-    var counter=setInterval(timer, 1000);               // set interval to 1 second
-    function timer() {                                  // timer function
-        count = count-1;                                // decrement var count by 1
-            if (count === 0) {                          // if the count gets to 0
-            clearInterval(counter);     
-            nextScreen();
-    
-    // display correct answer to HTML
-    // after 7 seconds transition to next screen
-    // nextScreen()
-
-}
-
-function nextScreen() {
-    recreate divs or re-add text to HTML
-    call timer
-    call questions and answers
-    call IF/ELSE function
-}
-
-function gameOn() {
-    countD();                                   // Call the countdown function
-    $(this).delay(999).queue(createWords);      // Delay the question/answers until the countdown pops up
-}
-
-function countD() {                                     // Function to count down
-    var count = 5;                                     // start counting at 30
-    var counter=setInterval(timer, 1000);               // set interval to 1 second
-    function timer() {                                  // timer function
-        count = count-1;                                // decrement var count by 1
-            if (count === 0) {                          // if the count gets to 0
-            clearInterval(counter);                     // clear the interval
-            $(".timer").html("<h2>Time's Up!</h2>");    // push 'time's up' to the HTML
-            $(".ques").remove();    
-            $(".ans").remove();    
-            correctAns();
-           
-            
-                                                        
-            return;
-            }
-            // } else if (click on a, b, c, or d) {
-            
-            // }
-        $(".timer").html("Time Remaining: " + count);
-        
-    }
-}
-
-function createWords() {
-    $(".ques").append("<h2>The question goes here</h2>");
-    $(".text1").append("<h4>Answer 1 goes here</h4>");
-    $(".text2").append("<h4>Answer 2 goes here</h4>"); 
-    $(".text3").append("<h4>Answer 3 goes here</h4>");
-    $(".text4").append("<h4>Answer 4 goes here</h4>"); 
-
-*/
-
-
-
-
-
-/*
-
-function countD() {                                     // Function to count down
-    var count = 5;                                     // start counting at 30
-    var counter=setInterval(timer, 1000);               // set interval to 1 second
-    function timer() {                                  // timer function
-        count = count-1;                                // decrement var count by 1
-            if (count === 0) {                          // if the count gets to 0
-            clearInterval(counter);                     // clear the interval
-            $(".timer").html("<h2>Time's Up!</h2>");    // push 'time's up' to the HTML
-            $(".ques").remove();    
-            $(".ans").remove();    
-            correctAns();
-           
-            
-                                                        // CALL FUNCTION TO TRANSITION TO THE NEXT SCREEN
-            return;
-            }
-            // } else if (click on a, b, c, or d) {
-            // CALL FUNCTION TO TRANSITON TO THE NEXT SCREEN 
-            // }
-        $(".timer").html("Time Remaining: " + count);
-        
-    }
-}
-
-function createWords() {
-    $(".ques").append("<h2>The question goes here</h2>");
-    $(".text1").append("<h4>Answer 1 goes here</h4>");
-    $(".text2").append("<h4>Answer 2 goes here</h4>"); 
-    $(".text3").append("<h4>Answer 3 goes here</h4>");
-    $(".text4").append("<h4>Answer 4 goes here</h4>"); 
-
-}
-
-function correctAns() {
-    $(".r2c1").append("<h2> Push the array or object info with the correct answer  </h2>");
-    $(".r2c1").css({
-    "background" : "white",
-    "text-align" : "center"
-});
-   // $(".ques").append("push the array or object info with the correct answer"); // .ques was removed in the previous function - just wipe the HTML
-}
-
-function gameOn() {
-    countD();                                   // Call the countdown function
-    $(this).delay(999).queue(createWords);      // Delay the question/answers until the countdown pops up
-}
-
-    
-
-
-
-$(".startB").on("click", function() {
-    gameOn();
-});
-
-*/
-
-
-
-
-
-
-
-
-
-
 
 
 
