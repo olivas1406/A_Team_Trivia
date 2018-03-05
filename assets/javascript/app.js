dc3093e5b531e62e75e5585bafcc3a9314a5c4e0
olivas1406 <olivas1406@msn.com>
2018-03-04 17:31:41 -0700

    starting easier homework assignment

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,12 @@
 
 
-    var currQuest = ["this is test question 1", "this is test question 2", "this is test question 3"];
-    var answer1 = ["answer 1-1", "answer 1-2", "answer 1-3"];
-    var answer2 = ["answer 2-1", "answer 2-2", "answer 2-3"];
-    var answer3 = ["answer 3-1", "answer 3-2", "answer 3-3"];
-    var answer4 = ["answer 4-1", "answer 4-2", "answer 4-3"];
-    var unAns = 0;               // variable to hold the number of unanswered questions
-    var corAns = 0;              // variable to hold the number of correclty answered questions
-    var notCorAns = 0;         // variable to hold the number of incorrectly answered questions
-    var ansYes = 1; 
+
+
 
 $(document).ready(function(){ 
 
     function timeMe() {
-        var count = 8;                                      // start counting from 30, 8 for testing
+        var count = 5;                                      // start counting from 30
         var counter=setInterval(timer, 1000);               // set interval to 1 second
         function timer() {                                  // timer function
             count = count-1;                                // decrement var count by 1
@@ -21,8 +14,7 @@ $(document).ready(function(){
                 clearInterval(counter);                     // clearInterval
                 $(".timer").empty();                    // empty the timer div
                 $(".texty").html("Time's up");                      // push 'time's up' to the HTML 
-                $(".ques").empty();                         // empty the ques div
-                $(".ans").empty();                          // empty the ans div
+                                   // empty the ans div
               
                 correctAns();                               // call the correctAns function
                 }
@@ -30,39 +22,8 @@ $(document).ready(function(){
         }
     }
 
-    function askMe() {                                      // function to populate the questions/answers to the HTML
-        $(".ques").html(currQuest[0]);
-        $(".text1").html(answer1[0]);
-        $(".text2").html(answer2[0]);
-        $(".text3").html(answer3[0]);
-        $(".text4").html(answer4[0]);
-        
-    }
 
 
-    function correctAns() {
-        $(".ques").html("correct answer goes here");
-        var count = 6;                                      // start counting from 30, 8 for testing
-        var counter=setInterval(timer, 1000);               // set interval to 1 second
-        function timer() {                                  // timer function
-            count = count-1;                                // decrement var count by 1
-                if (count === 0) {                          // if the count gets to 0
-                clearInterval(counter);                     // clearInterval
-                $(".ques").empty();
-                $(".timer").empty();
-                timeMe();
-                $(this).delay(999).queue(askMe);
-                }
-                if (ansYes === 0) {
-                    notCorAns++;
-                } else if (ansYes === 1) {
-                    corAns++;
-                } else {
-                    unAns++;
-                }
-        }
-    }
-    
 
 
 
@@ -74,6 +35,7 @@ $(document).ready(function(){
 
 
 });
+
 
 
 
